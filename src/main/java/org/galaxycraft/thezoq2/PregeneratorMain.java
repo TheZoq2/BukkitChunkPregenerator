beef3c614c30a640377ad7aac1c3fa3846abecf0
TheZoq2 <frans.skarman@gmail.com>
2016-03-23 23:28:41 +0100

    Version one complete

--- src/main/java/org/galaxycraft/thezoq2/PregeneratorMain.java
+++ src/main/java/org/galaxycraft/thezoq2/PregeneratorMain.java
@@ -1,5 +1,6 @@
 package org.galaxycraft.thezoq2;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,6 +9,8 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.World;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.io.*;
 import java.util.logging.Level;
@@ -19,13 +22,15 @@ import java.util.logging.Level;
 
 public class PregeneratorMain extends JavaPlugin implements Listener
 {
-    static String STATUS_FILENAME = "genStatus";
+    static String PLUGIN_FOLDER = "plugins/pregenerator/";
+    static String STATUS_FILENAME = PLUGIN_FOLDER + "genStatus";
     int radius;
     int currentRadius;
-    int maxRadius = 15000;
+    int maxRadius = 430;
     int centerX = 0;
     int centerY = 0;
 
+    boolean running;
     int taskID;
 
     World world;
@@ -34,8 +39,13 @@ public class PregeneratorMain extends JavaPlugin implements Listener
     public void onEnable()
     {
         super.onEnable();
+        this.getServer().getPluginManager().registerEvents(this, this);
 
         getLogger().info("Loading pregenerator");
+
+        new File(PLUGIN_FOLDER).mkdirs();
+
+        readStatus();
 
         BukkitScheduler scheduler = this.getServer().getScheduler();
         //scheduler.scheduleSyncRepeatingTask(this, new RPGUpdateTask(this), 0L, 1L);
@@ -43,75 +53,104 @@ public class PregeneratorMain extends JavaPlugin implements Listener
         
         world = this.getServer().getWorld("world");
 
-        //Nobody is on, start generating chunks
-        if(getServer().getOnlinePlayers().size() == 0)
-        {
-            startGenerator();
-        }
+        checkGeneratorShouldRun(getServer().getOnlinePlayers().size());
+    }
+    @Override
+    public void onDisable()
+    {
+        super.onDisable();
+
+        writeStatus();
     }
 
     /*
-     * The generation will follow this pattern
-     *
-     *             currentRadius
-     *          __________________
-     *
-     *      ####******************####                       |
-     *      #                        #                       |
-     *      #                        #                       |
-     *      #                        #                       |
-     *      *                        *     |                 |
-     *      *                        *     |                 |
-     *      *           *            *     | currentRadius   | radius
-     *      *                        *     |                 |
-     *      *                        *     |                 |
-     *      #                        #                       |
-     *      #                        #                       |
-     *      #                        #                       |
-     *      ####******************####                       |
+     * The generation will follow this pattern. 
+        *
+        *             currentRadius
+        *            ________________
+        *
+        *      #####+++++++++++++++++####                        
+        *      #           *            #                        
+        *      #           *            #                        
+        *      +           *            +     |                  
+        *      +           *            +     |                  
+        *      +           *            +     |                  
+        *      +************************+     | currentRadius * 2  | radius
+        *      +           *            +     |                    |
+        *      +           *            +     |                    |
+        *      +           *            +                          |
+        *      #           *            #                          |
+        *      #           *            #                          |
+        *      ######++++++++++++++######                          |
+        *            ______________
+        *
+        *            currentRadius*2
      */
-    void startGenerator()
+    private void startGenerator()
     {
         this.getLogger().info("Starting generator");
+        running = true;
 
+        //This variable is needed by the repeating task in order to turn of the generation when it is done
+        PregeneratorMain thisPlugin = this;
         taskID = this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
             @Override
             public void run() {
                 int currX = radius;
                 int currY = currentRadius;
 
-                generateChunk(currX, currY);
-                generateChunk(-currX, currY);
-                generateChunk(currY, currX);
-                generateChunk(currY, -currX);
+                generateChunk(radius, -currentRadius);
+                generateChunk(radius, currentRadius);
+
+                generateChunk(-currentRadius, -radius);
+                generateChunk(currentRadius, -radius);
+
+                generateChunk(-radius, currentRadius);
+                generateChunk(-radius, -currentRadius);
+
+                generateChunk(currentRadius, radius);
+                generateChunk(-currentRadius, radius);
 
                 currentRadius++;
                 if(currentRadius > radius)
                 {
                     radius++;
                     currentRadius = 0;
+
+                    if(radius % 10 == 0)
+                    {
+                        Bukkit.getLogger().info("Generation radius: " + radius);
+                    }
                 }
 
-                getLogger().info("Generating: " + currX + " " + currY);
+                if(radius > maxRadius)
+                {
+                    Bukkit.getServer().getLogger().info("Max radius reached. Stopping generation");
+                    thisPlugin.pauseGenerator();
+                }
             }
         }, 4L, 4L);
     }
 
-    void stopGenerator()
+    private void pauseGenerator()
     {
         this.getServer().getScheduler().cancelTask(taskID);
 
+        this.getLogger().info("Pausing generator");
+
+        running = false;
         writeStatus();
     }
 
 
-    void writeStatus()
+    private void writeStatus()
     {
         try
         {
             BufferedWriter writer = new BufferedWriter(new FileWriter(STATUS_FILENAME));
 
             writer.append(Integer.toString(radius));
+            writer.append("\n");
             writer.append(Integer.toString(currentRadius));
 
             writer.close();
@@ -122,7 +161,7 @@ public class PregeneratorMain extends JavaPlugin implements Listener
         }
     }
 
-    void readStatus()
+    private void readStatus()
     {
         try
         {
@@ -131,6 +170,8 @@ public class PregeneratorMain extends JavaPlugin implements Listener
             radius = Integer.parseInt(reader.readLine());
             currentRadius = Integer.parseInt(reader.readLine());
             reader.close();
+
+            getServer().getLogger().info("Pregenerator starting from radius " + radius);
         }
         catch(IOException e)
         {
@@ -140,12 +181,39 @@ public class PregeneratorMain extends JavaPlugin implements Listener
         }
     }
 
-    void generateChunk(int chunkX, int chunkY)
+    private void generateChunk(int chunkX, int chunkY)
     {
         if(!world.isChunkLoaded(chunkX, chunkY))
         {
             world.loadChunk(chunkX, chunkY);
             world.unloadChunk(chunkX, chunkY);
         }
+    }
+
+    private void checkGeneratorShouldRun(int playerAmount)
+    {
+        if(playerAmount == 0)
+        {
+            if(!running)
+            {
+                startGenerator();
+            }
+        }
+        else
+        {
+            pauseGenerator();
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e)
+    {
+        checkGeneratorShouldRun(this.getServer().getOnlinePlayers().size());
+    }
+    
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e)
+    {
+        checkGeneratorShouldRun(this.getServer().getOnlinePlayers().size() - 1);
     }
 }
